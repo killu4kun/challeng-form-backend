0f9c1e75e84e4633d56f2a3d8b5814f395a2d179
killu4kun <mateeus.soarees@outlook.com>
2025-02-17 19:00:17 -0300

    setup inicial do mongo

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FormModule } from './form/form.module';
+import { MongoModule } from './database/mongo/mongo.module';
 
 @Module({
-  imports: [],
+  imports: [FormModule, MongoModule],
   controllers: [AppController],
   providers: [AppService],
 })
