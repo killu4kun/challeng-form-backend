1495dbbdd178ce8c67641336a91e39a64769f89c
killu4kun <mateeus.soarees@outlook.com>
2025-02-18 12:01:51 -0300

    adiciona permissão do cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,9 +4,14 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    Credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
