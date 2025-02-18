4ca3e4b6298f04896418a009da1f2291dc9a6e02
killu4kun <mateeus.soarees@outlook.com>
2025-02-17 23:52:01 -0300

    adiciona form schema

--- backend/src/form/form.module.ts
+++ backend/src/form/form.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FormController } from './form.controller';
 import { FormService } from './form.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FormSchema } from './schemas/form.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'Form', schema: FormSchema }])],
   controllers: [FormController],
   providers: [FormService],
 })
---
+++ backend/src/form/schemas/form.schema.ts
@@ -0,0 +1,10 @@
+import { Schema, Document } from 'mongoose';
+import { SubmitFormDto } from '../submit-form.dto';
+
+export const FormSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  cep: { type: String, required: true },
+});
+
+export interface FormDocument extends SubmitFormDto, Document {}
